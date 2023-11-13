9955b374702ae7ffed5f494e227e19fe2badce55
Abhi5453Abhi <40296575+Abhi5453Abhi@users.noreply.github.com>
2023-11-13 13:05:45 +0530

    Time: 11 ms (36.64%), Space: 12.2 MB (64.21%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
+
+    /*
+
+    ["2","1","/","3","*"]
+
+    9
+
+    */
     int evalRPN(vector<string>& tokens) {
-        stack<int> s;
-        for(int i=0;i<tokens.size();i++){
-            if(tokens[i] == "/" || tokens[i] == "*" ||tokens[i] == "+" ||tokens[i] == "-"){
-                    int second = s.top();
-                    s.pop();
-                    int first = s.top();
-                    s.pop();
-                    int no=0;
-                    char ch = tokens[i][0];
-                    cout<<ch<<" ";
-                    switch(ch){
-                        case '/': 
-                        no = first/second;  
-                        break; 
-                        case '*': 
-                        no = first*second;  
-                        break; 
-                        case '-': 
-                        no = first-second;  
-                        break; 
-                        case '+': 
-                        no = first+second;  
-                        break; 
-                    }
-                    s.push(no);
-            }else{
-                s.push(stoi(tokens[i]));
-            }
-        }
-        return s.top();
+       stack<int> s;
+       for(int i=0;i<tokens.size();i++){
+           if(tokens[i] == "/" || tokens[i] == "*" || tokens[i] == "-" || tokens[i] == "+"){
+               int first = s.top();
+               s.pop();
+               int second = s.top();
+               s.pop();
+               int newNumber;
+               if(tokens[i] == "/"){
+                   newNumber = second/first;
+               }
+               if(tokens[i] == "+"){
+                   newNumber = second+first;
+               }
+               if(tokens[i] == "-"){
+                   newNumber = second-first;
+               }
+               if(tokens[i] == "*"){
+                   newNumber = second*first;
+               }
+               s.push(newNumber);
+           }else{
+               s.push(stoi(tokens[i]));   
+           }
+       }
+       return s.top();
     }
 };
